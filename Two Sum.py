7e25e910029f7e9e16d1973843e5f9bbd473c5ad
Jay Dong <51755112+jdong135@users.noreply.github.com>
2020-12-18 13:27:41 -0600

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -8,3 +8,18 @@ class Solution:
                 return [i, nums_dict[complement]]
             else:
                 nums_dict[nums[i]] = i
+
+                
+# If the list is already sorted, you can use the following appraoch with two pointers
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            sum = numbers[left] + numbers[right]
+            if sum == target:
+                return [left+1,right+1]
+            elif sum < target:
+                left += 1
+            else:
+                right -= 1
